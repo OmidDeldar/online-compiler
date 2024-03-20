40851890dda7466688c6874c50d08d8dbac0a11b
Omid Deldar <omiddeldar1380.od@gmail.com>
2024-03-20 14:47:26 +0330

    task => change port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger as NestLogger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const nestLogger = new NestLogger('Main_Logger');
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
   .setTitle('online-compiler')
@@ -11,6 +13,10 @@ async function bootstrap() {
   .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  // await app.listen(3000);
+  const port = 4600;
+  await app.listen(port).then(async () => {
+    nestLogger.log(`http://127.0.0.1:${port}/api`, 'Running Swagger');
+  });
 }
 bootstrap();
