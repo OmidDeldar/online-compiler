594355107f0b6e942d964dceaf39c622e3fef274
Omid Deldar <omiddeldar1380.od@gmail.com>
2024-03-25 10:16:10 +0430

    task => add mongoose

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Judge0Module } from '../judge0/judge0.module';
 import { MongoModule } from 'src/utility/mongo/mongo.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     Judge0Module,
+    MongooseModule.forRoot('mongodb://localhost:32768/compiler'),
     MongoModule
   ],
   controllers: [AppController],
